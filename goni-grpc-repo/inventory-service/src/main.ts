8b421b778db0c26ca21189e56f6203cc3358eecb
goni715 <halesgoni715149123@gmail.com>
2026-05-16 21:21:08 +0600

    grpc workplace set-up

---
+++ goni-grpc-repo/inventory-service/src/main.ts
@@ -0,0 +1,33 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app/app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        package: 'inventory',
+        protoPath: join(process.cwd(), 'libs/proto/inventory.proto'),
+        
+      },
+    },
+  );
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
+  );
+}
+
+bootstrap();
